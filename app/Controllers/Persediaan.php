e278bba2365f8090e61c76c508e6f4874701bb86
dydef23 <dianayap.dy@gmail.com>
2022-06-19 01:21:03 +0700

    Data exp done

--- app/Controllers/Persediaan.php
+++ app/Controllers/Persediaan.php
@@ -47,20 +47,24 @@ class Persediaan extends BaseController
         $session = session();
         
         $id = $this->request->getVar('medId');
-        
         $filter = $this->request->getVar('filter');
+        $name = $this->request->getVar('medName'); 
 
         $medicine = $this->persediaanModel->findAll();
         
+        if(empty($id)){
+            
+        }
+
+
         if($filter == "1"){
-            $name = $this->request->getVar('medName'); 
             $getSearch = $this->persediaanModel->getSearch($name);
             if(empty($getSearch)){
                 $data = [
                     'data' => $medicine,
                     'medicine' => $medicine
                 ];
-                // $session->setFlashData('msg', 'Obat tidak ditemukan');
+                $session->setFlashData('msg', 'Obat tidak ditemukan');
             }else {
                 $data = [
                     'data' => $getSearch,
