b02651519fea344740f3143c313d907ee69a6271
dydef23 <dianayap.dy@gmail.com>
2022-06-27 15:04:21 +0700

    Update Opname

--- app/Views/persediaan/opname.php
+++ app/Views/persediaan/opname.php
@@ -3,28 +3,41 @@
     <div class="col">
       <h2 class="mb-2">Opname Stok</h2>
       
-      <form class="mt-4 mb-4">
+      <form class="mt-4 mb-4" method="POST" id="formDataExp" action="/persediaan/getOpname">
+        <label class=" col-form-label" for="">Cari Berdasarkan ID Obat</label>
+        <?php 
+            if(session()->getFlashdata('msg') != NULL):       
+          ?>
+          <div class="col-sm-3 my-1">
+            <label class="sr-only" for="hidden">Hidden</label>
+            <input type="hidden" class="form-control alert" id="msg" name="msg" placeholder="Message" value="<?= session()->getFlashdata('msg'); ?>">
+          </div>
+        <?php endif;?>
         <div class="form-row align-items-center">
-            <div class="col-sm-3 my-1">
+          <div class="col-sm-3 my-1">
             <label class="sr-only" for="medId">ID</label>
-            <input type="text" class="form-control" id="medId" placeholder="ID Obat" data-toggle="modal" data-target="#exampleModalCenter">
-            </div>
-            <div class="col-sm-3 my-1">
+            <input type="text" class="form-control" id="medId" name="medId" placeholder="ID Obat" data-toggle="modal" data-target="#exampleModalCenter" required>
+          </div>
+          <div class="col-sm-3 my-1">
             <label class="sr-only" for="medName">Nama Obat</label>
             <div class="input-group">
-                <input type="text" class="form-control" id="medName" placeholder="Nama Obat" data-toggle="modal" data-target="#exampleModalCenter" >
+                <input type="text" class="form-control" id="medName" placeholder="Nama Obat" name="medName" >
             </div>
-            </div>
-            <div class="col-auto my-1">
-              <button type="submit" class="btn btn-primary">Submit</button>
-            </div>
-            <div class="col-auto my-1">
-              <button type="submit" class="btn btn-danger" id="clearBtn">Clear</button>
-            </div>
+          </div>
+          <div class="col-auto my-1">
+            <button type="submit" class="btn btn-primary msg" name="submitDataExp">Submit</button>
+          </div>
+          <div class="col-auto my-1">
+            <button type="submit" class="btn btn-danger" id="clearBtn" >Clear</button>
+          </div>
         </div>
-    </form>
+        <div class="col-sm-6 my-1 ms-1">
+            <input type="checkbox" class="form-check-input" id="filter" name="filter">
+            <label class="form-check-label" for="exampleCheck1">Filter Berdasarkan Nama</label>
+        </div>
+      </form>
 
-        <table id="example2" class="table table-hover">
+        <table id="tableData" class="table table-hover">
             <thead>
             <tr>
                 <th scope="col">Tanggal</th>
@@ -36,13 +49,48 @@
                 <th scope="col">Stok</th> 
             </tr>
             </thead>
-            <tbody>
+            <tbody> 
+              <?php $index=0; ?>
               <?php foreach ($data as $p) : ?>
                 <tr>
-                    <th scope="row"><?= $p['medicine_id']; ?></th>
-                    <td><?= $p['medicine_name']; ?></td>
-                    <td><?= $p['medicine_name']; ?></td>
+                  <th scope="row"><?= $p['created_at']; ?></th>
+                  <td><?= $p['stock_invoice']; ?></td>
+                  <td>
+                    <?php 
+                    if($p['stock_type'] == 'P'){  
+                      echo 'Penyesuaian Stock';
+                    }else if ($p['stock_type'] == 'I'){
+                      echo 'Barang Masuk';
+                    }else{
+                      echo 'Barang Keluar';
+                    }
+                    ?>
+                  </td>
+                  <td><?= $p['item_price']; ?></td>
+                  <td>
+                    <?php 
+                      if($p['stock_type'] == 'I'){
+                        echo $p['stock_qty'] - $index;
+                      }else if($p['stock_qty'] > $index){
+                        echo $p['stock_qty'] - $index;
+                      }else {
+                        echo '-';
+                      }
+                    ?>
+                  </td>
+                  <td>
+                    <?php  
+                      if($p['stock_type'] == 'O' || $p['stock_qty'] < $index){
+                        echo $index -  $p['stock_qty'];
+                      }else {
+                        echo '-';
+                      }
+                    ?>
+                    
+                  </td>
+                  <td><?= $p['stock_qty']; ?></td>
                 </tr>
+                <?php $index = $p['stock_qty']; ?>
               <?php endforeach; ?>
             </tbody>
             </tfoot>
@@ -56,7 +104,7 @@
   <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-lg" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+        <h5 class="modal-title" id="exampleModalLongTitle">Daftar Obat</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
@@ -71,7 +119,7 @@
                 </tr>
             </thead>
             <tbody>
-                <?php foreach ($data as $p) : ?>
+                <?php foreach ($stock as $p) : ?>
                     <tr data-dismiss="modal">
                         <th scope="row"><?= $p['medicine_id']; ?></th>
                         <td><?= $p['medicine_name']; ?></td>
@@ -90,3 +138,18 @@
 </div>
 
 <script src="<?= base_url('template/js/persediaan.js'); ?>"></script>
+
+<script>
+  $(function() {
+    $('#tableData').DataTable({
+        "paging": true,
+        "lengthChange": false,
+        "searching": false,
+        "ordering": true,
+        "info": true,
+        "autoWidth": false,
+        "responsive": true,
+    });
+  });
+
+</script>
