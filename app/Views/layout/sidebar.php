5408752764eeb0208e7d78f16bb3fef80b116ec6
Ananda Dharma <gotcrazy@asia.com>
2022-06-29 08:40:47 +0700

    light sidebar

--- app/Views/layout/sidebar.php
+++ app/Views/layout/sidebar.php
@@ -1,8 +1,8 @@
 <!-- Main Sidebar Container -->
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
+<aside class="main-sidebar sidebar-light-primary elevation-4">
   <!-- Brand Logo -->
   <a href="/" class="brand-link">
-    <img src="<?= base_url('template/img/AdminLTELogo.png'); ?>" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+    <img src="<?= base_url('template/img/AdminLTELogo.png'); ?>" alt="AdminLTE Logo" class="brand-image" style="opacity: .8">
     <span class="brand-text font-weight-normal text-uppercase">rsud jombang</span>
   </a>
 
