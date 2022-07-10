acc71946eed71fb7c7f4f5960c6ae5f9aedfe36f
dydef23 <dianayap.dy@gmail.com>
2022-07-10 19:39:59 +0700

    Update login

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -13,58 +13,57 @@ class Login extends BaseController
     }
     public function index()
     {
-        $login = $this->loginModel->findAll();
+        if (!empty($this->request->getPost())) {
+            $session = session();
+            $user = new LoginModel();
+            $username = $this->request->getVar('username');
+            $password = $this->request->getVar('password');
 
-        $data = [
-            'data' => $login,
-            'validation' => \Config\Services::validation()
-        ];
+            if (!$this->validate([
 
-        echo view('login', $data);
-    }
+                'username' => 'required',
+                'password' => 'required'
 
-    public function loginAuth()
-    {
-        $session = session();
-        $user = new LoginModel();
-        $username = $this->request->getVar('username');
-        $password = $this->request->getVar('password');
+            ])) {
+                $validation = \Config\Services::validation();
+                return redirect()->to(base_url('/login'))->withInput()->with('validation', $validation);
+            }
 
-        if (!$this->validate([
+            $data = $user->where('employee_username', $username)->first();
 
-            'username' => 'required',
-            'password' => 'required'
-
-        ])) {
-            $validation = \Config\Services::validation();
-            return redirect()->to(base_url('/login'))->withInput()->with('validation', $validation);
-        }
-
-        $data = $user->where('employee_username', $username)->first();
-
-        if ($data) {
-            if (strtolower($data['employee_departemen']) == 'farmasi') {
-                $pass = $data['employee_pass'];
-                $authPass = password_verify($password, $pass);
-                if ($password == $pass) {
-                    $ses_data = [
-                        'id' => $data['employee_id'],
-                        'name' => $data['employee_name'],
-                        'isLoggedIn' => TRUE
-                    ];
-                    $session->set($ses_data);
-                    return redirect()->to(base_url('/'));
+            if ($data) {
+                if (strtolower($data['employee_departemen']) == 'farmasi') {
+                    $pass = $data['employee_pass'];
+                    $authPass = password_verify($password, $pass);
+                    if ($password == $pass) {
+                        $ses_data = [
+                            'id' => $data['employee_id'],
+                            'name' => $data['employee_name'],
+                            'isLoggedIn' => TRUE
+                        ];
+                        $session->set($ses_data);
+                        return redirect()->to(base_url('/'));
+                    } else {
+                        $session->setFlashData('msg', 'Password anda salah');
+                        return redirect()->to(base_url('/login'));
+                    }
                 } else {
-                    $session->setFlashData('msg', 'Password anda salah');
+                    $session->setFlashData('msg', 'Anda tidak memiliki hak akses terhadap sistem');
                     return redirect()->to(base_url('/login'));
                 }
             } else {
-                $session->setFlashData('msg', 'Anda tidak memiliki hak akses terhadap sistem');
+                $session->setFlashData('msg', 'Username tidak ditemukan');
                 return redirect()->to(base_url('/login'));
             }
-        } else {
-            $session->setFlashData('msg', 'Username tidak ditemukan');
-            return redirect()->to(base_url('/login'));
+        }else {
+            $login = $this->loginModel->findAll();
+            $session = session();
+            $session->destroy();
+            $data = [
+                'data' => $login,
+                'validation' => \Config\Services::validation()
+            ];
+            echo view('login', $data);
         }
     }
 
